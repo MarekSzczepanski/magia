a327f4ed6852d2b139c0c4c50b256fc1d4ef7553
Marek <marek@ilieth.at>
2023-02-15 15:55:57 +0100

    Add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,8 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from 'styled-components'
+import { Link } from "gatsby"
+import { Stars } from '../gatsbyImages'
 
 import Header from "./header"
 import "./layout.css"
@@ -15,12 +18,26 @@ const Layout = ({ children }) => {
     }
   `)
 
+  const P = styled.p`
+    padding: 0;
+    font-size: 1.2vw;
+    font-weight: 800;
+    text-align: center;
+  `
+  const B = styled.span`
+   
+  `
+
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}
+          <P>Spotkajmy się osobiście - <B>Ostróda</B>, <B>Iława</B>, <B>Olsztyn!</B></P>
+          <Stars width={'2vw'}></Stars>
+          <P>Zdalnie - <B>cała Polska!</B></P>
+          <Stars width={'2vw'}></Stars>
+          <P>© {new Date().getFullYear()} - <Link to='/' rel='noopener noreferer' activeStyle={{color: '#1a1e23', fontSize: '1.2vw', fontWeight: '800'}}>magiainternetu.com</Link></P>
         </footer>
     </>
   )
