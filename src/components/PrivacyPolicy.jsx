97665ebb361a9cc60ddf185203b5b1cf77dc48d8
Marek <marek@ilieth.at>
2023-03-26 13:44:18 +0200

    Add privacy policy and cookies pop up

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import styled from 'styled-components'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import Cookies from 'universal-cookie';
 
@@ -8,7 +8,6 @@ const Wrap = styled.div`
   position: fixed;
   bottom: 4.5vw;
   left: 0;
-  display: ${props => props.isPrivacyVisible ? 'flex' : 'none'};
   justify-content: space-between;
   width: 42vw;
   height: 6vw;
@@ -17,6 +16,13 @@ const Wrap = styled.div`
   border-left: 0;
   background-color: #ff0420;
   z-index: 3;
+  @media (orientation: portrait) {
+    bottom: 0vw;
+    width: 100vw;
+    height: 27vw;
+    padding: 1.5vw;
+    border-right: 0;
+  }
 `
 const Span = styled.span`
   width: 60%;
@@ -24,6 +30,10 @@ const Span = styled.span`
   line-height: 1.5vw;
   letter-spacing: .05vw;
   color: #fff;
+  @media (orientation: portrait) {
+    font-size: 3.5vw;
+    line-height: 4.5vw;
+  }
 `
 const ButtonsParent = styled.div`
   display: flex;
@@ -37,23 +47,36 @@ const StyledLink2 = styled.span`
     font-weight: 300;
     color: #ff0420;
   }
+  @media (orientation: portrait) {
+    height: 10vw !important; 
+    > a {
+      font-size: 3vw;
+    }
+  }
 `
 const Button = styled.button`
   width: 100%;
   height: 2vw !important;
   font-size: .9vw;
+  @media (orientation: portrait) {
+    height: 10vw !important; 
+    font-size: 3vw;
+  }
 `
 
 const PrivacyPolicy = () => {
     const cookies = new Cookies();
-    const [isPrivacyVisible, setIsPrivacyVisible] = useState(!cookies.get('privacyPolicy'));
+    const [isPrivacyVisible, setIsPrivacyVisible] = useState(null);
 
+    useEffect(() => setIsPrivacyVisible(cookies.get('privacyPolicy')), []);
+    
     const acceptPolicy = () => {
-        setIsPrivacyVisible(false);
-        cookies.set('privacyPolicy', 'accepted');
+      cookies.set('privacyPolicy', 'accepted');
+      setIsPrivacyVisible('accepted');
     } 
+
     return (
-        <Wrap isPrivacyVisible={isPrivacyVisible}>
+        <Wrap className={`cookies ${isPrivacyVisible}`}>
             <Span>Nasza witryna stosuje pliki cookies. Określenie warunków przechowywania i dostępu do plików cookies jest możliwe w przeglądarce.</Span>
             <ButtonsParent>
                 <StyledLink2 className='button button4'><Link to='/polityka-prywatnosci'>Dowiedz się więcej</Link></StyledLink2>
